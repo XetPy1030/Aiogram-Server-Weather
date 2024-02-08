6903604859129bc562547f811f6f9a1686e41198
XetPy1030 <xetpy1030@mail.ru>
2024-02-08 19:07:59 +0300

    State clear for start

--- handlers/start.py
+++ handlers/start.py
@@ -1,4 +1,5 @@
 from aiogram import Router, filters, types
+from aiogram.fsm.context import FSMContext
 
 from markups.weather import find_weather_kb
 
@@ -6,5 +7,6 @@ router = Router()
 
 
 @router.message(filters.CommandStart())
-async def cmd_start(message: types.Message):
+async def cmd_start(message: types.Message, state: FSMContext):
     await message.answer("Hello! I'm a bot!", reply_markup=find_weather_kb)
+    await state.clear()
